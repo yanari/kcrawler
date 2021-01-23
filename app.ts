da623d622ff71be69912504c8ecdc8f5e4327b46
yanari <yanarimy@gmail.com>
2021-01-23 07:47:40 -0300

    getting members names

--- app.ts
+++ app.ts
@@ -1,18 +1,26 @@
-const Nightmare = require('nightmare');
+// const Nightmare = require('nightmare');
+import axios from 'axios';
+import { JSDOM } from 'jsdom';
 
-const nightmare = Nightmare({show: true});
+import { Idol } from './models/idol';
 
-const parse = () => {
-  const children = document.querySelector('.entry-content').children;
-  const idols = Array.from(children).map((child) => {
-    return child.textContent;
-  });
-  return idols;
+
+const parse = (data: string) => {
+  const { document } = new JSDOM(data).window;
+  const body = document.querySelector('.entry-content');
+  const spans = body.querySelectorAll('span');
+
+  const members = Array.from(spans).filter(span => {
+    if (span.textContent.match('Stage Name')) return spans;
+  }).map(el => new Idol(el.nextSibling.textContent.trim()));
+  return members;
+
+  // const stageNameRegex = new RegExp(/Stage Name:<\/span>(.*?)</i);
+  // const stageName = stageNameRegex.exec(body)[1].trim();
+  // return stageName;
 };
 
-nightmare
-  .goto('https://kprofiles.com/loona-members-profile/')
-  .evaluate(parse)
-  .end()
-  .then(console.log)
-  .catch((err) => console.log('Failed:', err));
+(async () => {
+  const response = await axios.get('https://kprofiles.com/loona-members-profile/');
+  console.log(parse(response.data));
+})();
--- models/idol.ts
+++ models/idol.ts
@@ -1,5 +1,17 @@
-class Idol {
-  constructor() {
-    
+type Stat = 'Stage name' | 'Zodiac Sign' | 'Height';
+
+export class Idol {
+  name: string;
+  height: number;
+  zodiacSign: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  addZodiacSign(tag: HTMLSpanElement) {
+    if (tag.textContent.match('Zodiac Sign')) {
+      this.zodiacSign = tag.nextSibling.textContent.trim();
+    }
   }
 }
